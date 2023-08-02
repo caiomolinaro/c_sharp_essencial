167250ba61e5276028866fa8d4e80d8a4696fb98
Caio Molinaro <cmolinaro19@gmail.com>
2023-08-02 18:50:23 -0300

    Add: aulas sobre for, break e continue

---
+++ CSEstruturas_Controle/break_continue/Program.cs
@@ -0,0 +1,23 @@
+﻿//for(; ; )
+//{
+//    Console.WriteLine("Tecle algo (x) sai");
+//    var opcao = Console.ReadLine();
+
+//    Console.WriteLine(opcao.ToUpper());
+
+//    if ( opcao == "X" ||  opcao == "x")
+//    {
+//        Console.WriteLine("Tchau");
+//        break;
+//    }
+//}
+
+for(int i = 1; i < 10; i++)
+{
+    if (i == 4)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+Console.ReadKey();
---
+++ CSEstruturas_Controle/for/Program.cs
@@ -0,0 +1,20 @@
+﻿//int resultado, numero;
+
+//Console.WriteLine("Digite o numero");
+//numero = Convert.ToInt32(Console.ReadLine());
+
+//for(int i = 1; i <= 10; i++)
+//{
+//    resultado = numero * i;
+//    Console.WriteLine(numero + "x" + i + "=" + resultado );
+//}
+
+for(int x = 0; x < 5; x++)
+{
+    for(int y = 0; y < 5; y++)
+    {
+        Console.WriteLine($"({x}, {y}) ");
+    }
+}
+
+Console.ReadKey();
