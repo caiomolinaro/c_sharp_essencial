b2831cb6bcc26051c5b57712e2024f2a49190b9a
Caio Molinaro <cmolinaro19@gmail.com>
2023-08-04 16:55:25 -0300

    Add: aulas sobre Classes, Objetos, Metodos e Parametros

---
+++ Classes_Metodos/Metodos/Program.cs
@@ -0,0 +1,18 @@
+﻿MinhaClasse minhaClasse = new MinhaClasse();
+
+minhaClasse.Saudacao();
+//minhaClasse.ExibirDataAtual();
+
+Console.ReadKey();
+
+class MinhaClasse
+{
+    public void Saudacao()
+    {
+        Console.WriteLine("Bem vindo");
+        ExibirDataAtual();
+    }
+
+    public void ExibirDataAtual() =>
+        Console.WriteLine(DateTime.Now.ToShortDateString());
+}
---
+++ Classes_Metodos/Parametros/Program.cs
@@ -0,0 +1,15 @@
+﻿MinhaClasse minhaClasse = new MinhaClasse();
+minhaClasse.Saudacao("Caio", DateTime.Now.ToShortDateString());
+
+
+Console.ReadKey();
+
+public class MinhaClasse
+{
+    public void Saudacao(string nome, string data)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(data);
+    }
+}
+
