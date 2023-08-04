27adb5fe505c2b88c5dafcbcb60d4ae646b027f1
Caio Molinaro <cmolinaro19@gmail.com>
2023-08-04 18:58:55 -0300

    Add: aulas sobre Construtor e This

---
+++ Classes_Metodos/Construtor/Program.cs
@@ -0,0 +1,29 @@
+﻿Aluno aluno = new Aluno("Caio", 22, "Masculino", "S");
+
+Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
+Console.WriteLine(aluno.Idade == 0 ? "null" : aluno.Idade);
+Console.WriteLine(aluno.Sexo == null ? "null" : aluno.Sexo);
+Console.WriteLine(aluno.Aprovado == null ? "null" : aluno.Aprovado);
+
+Console.ReadKey();
+
+public class Aluno
+{
+
+    public Aluno(string nome)=> Nome = nome;
+
+
+    public Aluno(string nome, int idade, string sexo, string aprovado):this(nome)
+    {
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+
+    }
+
+
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+}
---
+++ Classes_Metodos/This/Program.cs
@@ -0,0 +1,45 @@
+﻿Teste t1 = new Teste();
+t1.Num1 = 10;
+t1.Num2 = 20;
+t1.Exibir();
+
+Teste t2 = new Teste();
+t2.Num1 = 10;
+t2.Num2 = 20;
+t2.Exibir();
+
+
+Console.ReadKey();
+
+//public class Cliente
+//{
+//    public string? Nome;
+//    public int Idade;
+
+//    public Cliente(string? Nome, int Idade)
+//    {
+//        this.Nome = Nome;
+//        this.Idade = Idade;
+//    }
+//}
+
+
+
+
+public class Teste
+{
+    public int Num1;
+    public int Num2;
+
+    public void PassarParametro(Teste t)
+    {
+        Console.WriteLine($"this = {this}");
+        Console.WriteLine($"Num1:  = {t.Num1}");
+        Console.WriteLine($"Num2:  = {t.Num2}");
+    }
+
+    public void Exibir()
+    {
+        PassarParametro(this);
+    }
+}
