4883b70095972369ca1a0ac9732cce03bdd10dbc
Caio Molinaro <cmolinaro19@gmail.com>
2023-08-07 17:34:42 -0300

    Add: aulas sobre metodos com retorno

---
+++ Classes_Metodos/Metodos_Com_Retorno/Program.cs
@@ -0,0 +1,36 @@
+﻿Calculadora calc = new();
+
+Console.WriteLine(calc.Somar(10,10));
+Console.WriteLine(calc.Subtrair(10, 10));
+Console.WriteLine(calc.Multiplicar(10, 10));
+Console.WriteLine(calc.Dividir(10, 10));
+
+
+Console.ReadKey();
+
+public class Calculadora
+{
+    public int Somar(int x, int y)
+    {
+        return x + y;
+    }
+
+    public int Subtrair(int x, int y)
+    {
+        return x - y;
+    }
+
+    public int Multiplicar(int x, int y)
+    {
+        return x * y;
+    }
+
+    public int Dividir(int x, int y)
+    {
+        return x / y;
+    }
+
+
+}
+
+
