e9eabf567ed5635e007c4a88fa75ee685566ef46
Caio Molinaro <cmolinaro19@gmail.com>
2023-08-19 11:59:29 -0300

    Add: Aulas sobre List<T>

---
+++ Seção5_Arrays_List/Seção_5/ListT/Program.cs
@@ -0,0 +1,37 @@
+﻿List<string> nomes = new()
+{
+    "Caio","Beatriz","Douglas","Anderson"
+};
+
+foreach (string name in nomes)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine();
+
+nomes.Sort();
+
+foreach (string name in nomes)
+{
+    Console.WriteLine(name);
+}
+
+
+
+
+Console.ReadKey();
+
+//declarações da List<T>
+//<T> = T é o tipo da lista
+
+//List<string> lista;
+//lista = new List<string>();
+
+//List<int> lista1 = new List<int>();
+
+//var lista2 = new List<float>();
+
+//List<double> lista3 = new();
+
+
