27d4ae066180d47934610c502e537181dc61ca27
Caio Molinaro <cmolinaro19@gmail.com>
2023-08-21 20:38:49 -0300

    Add: aula sobre ArrayList e List<T>

---
+++ Seção5_Arrays_List/Seção_5/List_ArrayList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+ArrayList lista = new ArrayList();
+
+lista.Add(3);
+lista.Add(4);
+
+var elemento = lista[0];
+int numero = (int) lista[1];
+
+
+
+List<int> lista1 = new List<int>();
+lista1.Add(1);
+
+var item = lista1[0];
+
+Console.ReadKey();
+
+//A List<T> é mais segura e rápida que a ArrayList especialmente
+//quando os itens da lista são de tipo de valor
